a5a9e32579f2a685d0a038d6c6d8d2ff392f0529
civts <24255608+civts@users.noreply.github.com>
2024-04-09 23:15:15 +0200

    Can create a Secret in k8s

--- go.mod
+++ go.mod
@@ -35,16 +35,21 @@ require (
 	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/getsops/gopgagent v0.0.0-20170926210634-4d7ea76ff71a // indirect
 	github.com/getsops/sops/v3 v3.8.1 // indirect
 	github.com/go-jose/go-jose/v3 v3.0.0 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gobuffalo/flect v1.0.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
@@ -64,6 +69,7 @@ require (
 	github.com/hashicorp/vault/api v1.10.0 // indirect
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
@@ -75,6 +81,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
@@ -111,6 +118,7 @@ require (
 	k8s.io/apimachinery v0.29.3 // indirect
 	k8s.io/client-go v0.29.3 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
 	sigs.k8s.io/controller-tools v0.14.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -13,10 +14,14 @@ import (
 	orderedmap "github.com/wk8/go-ordered-map/v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
+	v1a "k8s.io/client-go/applyconfigurations/core/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+const MANAGED_BY = "Markhor"
 
 var kubeconfig string
 
@@ -38,7 +43,8 @@ func main() {
 	fmt.Println("Starting to watch the events in the cluster to see when markhorSecrets are created")
 	for event := range markhorSecrets.ResultChan() {
 		markhorSecret, ok := event.Object.(*v1.MarkhorSecret)
-		secretName := fmt.Sprintf("%s/%s", markhorSecret.ObjectMeta.Namespace, markhorSecret.ObjectMeta.Name)
+		namespace := markhorSecret.ObjectMeta.Namespace
+		secretName := fmt.Sprintf("%s/%s", namespace, markhorSecret.ObjectMeta.Name)
 		if !ok {
 			fmt.Println("Failed to cast the object to type MarkhorSecret")
 			continue
@@ -53,14 +59,77 @@ func main() {
 				continue
 			}
 
-			b, err := json.Marshal(decryptedData)
-			if err != nil {
-				fmt.Println("Error decrypted to json ", secretName)
-				continue
+			{ // Add managed-by annotation
+				annotation, present := getAnnotation(decryptedData)
+				metadata, ok := decryptedData.Get("metadata")
+				if !ok {
+					fmt.Println("Missing metadata in ", secretName)
+					continue
+				}
+				metadataObj, ok := metadata.(map[string]interface{})
+				if !ok {
+					fmt.Println("Missing metadata in ", secretName)
+					continue
+				}
+				annotations, ok := metadataObj["annotations"]
+				if !ok {
+					fmt.Println("Missing annotations in ", secretName)
+					annotations = make(map[string]interface{})
+				}
+				annotationsObj, ok := annotations.(map[string]interface{})
+				if !ok {
+					fmt.Println("Missing annotations in ", secretName)
+					annotationsObj = make(map[string]interface{})
+				}
+				if present {
+					annotationsObj[annotation] = MANAGED_BY
+				} else {
+					annotationsObj["markhor.example.com/managed-by"] = MANAGED_BY
+				}
+				metadataObj["annotations"] = annotationsObj
+				decryptedData.Set("metadata", metadataObj)
 			}
 
-			fmt.Println(string(b))
+			{ //Remove extra fields
+				decryptedData.Delete("markhorParams")
+				decryptedData.Delete("sops")
+				decryptedData.Set("apiVersion", "v1")
+				decryptedData.Set("kind", "Secret")
+			}
 
+			{ //Create new secret
+				// secret := &corev1.Secret{}
+				secret := &v1a.SecretApplyConfiguration{}
+
+				bytes, err := json.Marshal(decryptedData)
+				if err != nil {
+					fmt.Println("can't convert decrypted final to JSON:", err)
+					panic(err)
+				}
+				if err := json.Unmarshal(bytes, secret); err != nil {
+					fmt.Println("can't make secret from final JSON:", err)
+					panic(err)
+				}
+
+				clientset, err := kubernetes.NewForConfig(getK8sConfig())
+				if err != nil {
+					panic(err.Error())
+				}
+
+				// clientset.CoreV1().Secrets("").Watch(context.TODO(), metav1.ListOptions{})
+				// Apply the secret
+				fieldManager := "markhor"
+
+				_, err = clientset.CoreV1().Secrets(namespace).Apply(context.TODO(), secret, metav1.ApplyOptions{
+					FieldManager: fieldManager,
+				})
+				if err != nil {
+					fmt.Println("error creating the secret:", err)
+					//Apply failed with 1 conflict: conflict with>another fieldmanager has the secret
+				} else {
+					fmt.Println("new secret created correctly", secretName)
+				}
+			}
 		case watch.Modified:
 			fmt.Println("A MarkhorSecret was updated:", secretName)
 
@@ -69,6 +138,26 @@ func main() {
 		}
 	}
 	fmt.Println("Finished watching the events in the cluster. Most probably, the channel was closed")
+}
+
+func getAnnotation(decryptedData *orderedmap.OrderedMap[string, interface{}]) (string, bool) {
+	params, present := decryptedData.Get("markhorParams")
+	if !present {
+		return "", false
+	}
+	paramsObj, ok := params.(orderedmap.OrderedMap[string, interface{}])
+	if !ok {
+		return "", false
+	}
+	annotation, present := paramsObj.Get("managedAnnotation")
+	if !present {
+		return "", false
+	}
+	annotationStr, ok := annotation.(string)
+	if !ok {
+		return "", false
+	}
+	return annotationStr, true
 }
 
 func decryptMarkhorSecret(markhorSecret *v1.MarkhorSecret) (*orderedmap.OrderedMap[string, interface{}], error) {
