43cb59da1dfc8fb985c82dfad76a99caecf0105e
civts <24255608+civts@users.noreply.github.com>
2024-04-25 18:44:04 +0200

    Add ordering tests

--- pkg/decrypt/order.go
+++ pkg/decrypt/order.go
@@ -55,72 +55,122 @@ func sortJSONData(jsonData map[string]interface{}, eid slog.Attr) (*orderedmap.O
 		rawOrder[i] = s
 	}
 
-	ordering := Ordering{name: "", terms: parseOrdering(rawOrder, separator)}
+	o, err := parseOrdering(rawOrder, separator)
+	if err != nil {
+		return nil, errors.New(fmt.Sprint("could not parse the ordering:", err))
+	}
+	ordering := Ordering{Name: "", Terms: o}
 
-	return sortWithOrdering(jsonData, ordering, eid), nil
+	return sortWithOrdering(jsonData, ordering)
 }
 
-func sortWithOrdering(jsonData map[string]interface{}, ordering Ordering, eid slog.Attr) *orderedmap.OrderedMap[string, interface{}] {
+func sortWithOrdering(jsonData map[string]interface{}, ordering Ordering) (*orderedmap.OrderedMap[string, interface{}], error) {
 	om := orderedmap.New[string, interface{}]()
 
-	for _, k := range ordering.terms {
-		if len(k.terms) == 0 {
-			om.Set(k.name, jsonData[k.name])
-		} else {
-			data, ok := jsonData[k.name].(map[string]interface{})
-			if !ok {
-				slog.Error(fmt.Sprint("Error no key ", k.name, "in JSON"), eid)
+	for _, k := range ordering.Terms {
+		if len(k.Terms) == 0 {
+			data := jsonData[k.Name]
+			if data == nil {
+				return nil, errors.New(fmt.Sprint("Error no key ", k.Name, " in JSON"))
 			}
-			nestedObj := sortWithOrdering(data, k, eid)
-			om.Set(k.name, nestedObj)
+			om.Set(k.Name, data)
+		} else {
+			data, ok := jsonData[k.Name].(map[string]interface{})
+			if !ok {
+				return nil, errors.New(fmt.Sprint("Error no key ", k.Name, " in JSON"))
+			}
+			nestedObj, err := sortWithOrdering(data, k)
+			if err != nil {
+				return nil, err
+			}
+			om.Set(k.Name, nestedObj)
 		}
 	}
 
-	return om
+	return om, nil
 }
 
 type Ordering struct {
-	name  string
-	terms []Ordering
+	Name  string
+	Terms []Ordering
 }
 
-func parseOrdering(raw []string, separator string) []Ordering {
+const duplicatesErrorMessage = "has duplicates in it: "
+
+func parseOrdering(input []string, separator string) ([]Ordering, error) {
+	result, err := parseOrderingRec(input, separator)
+	if err != nil {
+		return nil, err
+	}
+	name, hasDuplicate := hasDuplicates(result, separator)
+	if hasDuplicate {
+		return nil, errors.New(fmt.Sprint(duplicatesErrorMessage, name))
+	}
+	return result, nil
+}
+
+func parseOrderingRec(input []string, separator string) ([]Ordering, error) {
 	result := make([]Ordering, 0)
-	lr := len(raw)
-	for i := 0; i < lr; i++ {
-		s := raw[i]
+	inputLen := len(input)
+	for i := 0; i < inputLen; i++ {
+		s := input[i]
 		parts := strings.SplitN(s, separator, 2)
-		if len(parts) > 1 {
+		if len(parts) > 1 { //It is a nested element
 			name := parts[0]
 			terms := make([]string, 1)
 			terms[0] = parts[1]
 			for {
 				i++
-				if i >= lr {
+				if i >= inputLen {
 					break
 				}
-				s2 := raw[i]
-				parts := strings.SplitN(s2, separator, 2)
-				if parts[0] != name {
+				s2 := input[i]
+				parts2 := strings.SplitN(s2, separator, 2)
+				if parts2[0] != name {
 					i--
 					break
+				} else if len(parts2) != 2 {
+					return nil, fmt.Errorf("found one element %s missing %s%s", s2, separator, parts[1])
 				}
-				terms = append(terms, parts[1])
+				terms = append(terms, parts2[1])
+			}
+			nestedOrdering, err := parseOrderingRec(terms, separator)
+			if err != nil {
+				return nil, errors.New(fmt.Sprint(name, separator, err))
 			}
 			newOrdering := Ordering{
-				name:  name,
-				terms: parseOrdering(terms, separator),
+				Name:  name,
+				Terms: nestedOrdering,
 			}
 			result = append(result, newOrdering)
-		} else {
+		} else { //We reached a leaf
 			newOrdering := Ordering{
-				name:  s,
-				terms: make([]Ordering, 0),
+				Name:  s,
+				Terms: make([]Ordering, 0),
 			}
 			result = append(result, newOrdering)
 		}
 	}
-	return result
+	return result, nil
+}
+
+// Checks if an ordering has duplicate keys in it
+func hasDuplicates(slice []Ordering, separator string) (string, bool) {
+	seen := make(map[string]struct{})
+
+	for _, value := range slice {
+		name := value.Name
+		if _, ok := seen[name]; ok {
+			return name, true
+		}
+		seen[name] = struct{}{}
+		n, h := hasDuplicates(value.Terms, separator)
+		if h {
+			return name + separator + n, true
+		}
+	}
+
+	return "", false
 }
 
 // func sortAlphabetically(data map[string]interface{}) map[string]interface{} {
---
+++ pkg/decrypt/order_test.go
@@ -0,0 +1,457 @@
+package decrypt
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	orderedmap "github.com/wk8/go-ordered-map/v2"
+)
+
+func TestParseOrderingRec_Valid(t *testing.T) {
+	t.Parallel()
+	raw := []string{
+		"apiVersion",
+		"kind",
+		"metadata/name",
+		"metadata/namespace",
+		"markhorParams/hierarchySeparator",
+		"markhorParams/managedAnnotation",
+		"markhorParams/order",
+		"type",
+		"data/sessionSecret",
+		"stringData/another",
+	}
+
+	expectedResult := []Ordering{
+		{Name: "apiVersion", Terms: []Ordering{}},
+		{Name: "kind", Terms: []Ordering{}},
+		{Name: "metadata", Terms: []Ordering{
+			{Name: "name", Terms: []Ordering{}},
+			{Name: "namespace", Terms: []Ordering{}},
+		}},
+		{Name: "markhorParams", Terms: []Ordering{
+			{Name: "hierarchySeparator", Terms: []Ordering{}},
+			{Name: "managedAnnotation", Terms: []Ordering{}},
+			{Name: "order", Terms: []Ordering{}},
+		}},
+		{Name: "type", Terms: []Ordering{}},
+		{Name: "data", Terms: []Ordering{
+			{Name: "sessionSecret", Terms: []Ordering{}},
+		}},
+		{Name: "stringData", Terms: []Ordering{
+			{Name: "another", Terms: []Ordering{}},
+		}},
+	}
+
+	result, err := parseOrderingRec(raw, "/")
+	if err != nil {
+		t.Fatal("This error whould have not occcourred", err)
+	}
+	diff := cmp.Diff(expectedResult, result)
+	if diff != "" {
+		t.Errorf("%s", diff)
+	}
+}
+
+func TestParseOrderingRec_Invalid(t *testing.T) {
+	t.Parallel()
+	inputs := [][]string{
+		{
+			"apiVersion",
+			"kind",
+			"metadata/name",
+			"metadata/namespace",
+			"markhorParams/hierarchySeparator",
+			"markhorParams/managedAnnotation",
+			"markhorParams/order",
+			"markhorParams", //culprit
+			"type",
+		},
+		{
+			"apiVersion",
+			"kind",
+			"metadata/name",
+			"metadata/namespace",
+			"markhorParams/hierarchySeparator",
+			"markhorParams/managedAnnotation",
+			"markhorParams/order/nested", //culprit
+			"markhorParams/order",
+			"type",
+		},
+	}
+	for i, input := range inputs {
+		innerinput := input
+		t.Run(fmt.Sprint("Input number ", i, " (starting form 0)"), func(t *testing.T) {
+			t.Parallel()
+			_, err := parseOrderingRec(innerinput, "/")
+			if err == nil {
+				t.Fatal("This operation should have failed")
+			}
+		})
+	}
+}
+
+func TestParseOrdering_Invalid_Check_Error_Duplicate(t *testing.T) {
+	t.Parallel()
+	culprit := "markhorParams/hierarchySeparator"
+	raw := []string{
+		"apiVersion",
+		"kind",
+		"metadata/name",
+		"metadata/namespace",
+		"markhorParams/hierarchySeparator",
+		"markhorParams/managedAnnotation",
+		"markhorParams/order",
+		culprit,
+		"type",
+	}
+	_, err := parseOrdering(raw, "/")
+	errorMesage := err.Error()
+	if !strings.Contains(errorMesage, duplicatesErrorMessage) {
+		t.Fatal("Error message should say it found a duplicate, but instead reads: ", err)
+	}
+	if !strings.Contains(errorMesage, culprit) {
+		t.Fatal("Error message should contain the culprit item, but instead reads: ", err)
+	}
+}
+
+func TestParseOrdering_Valid(t *testing.T) {
+	t.Parallel()
+	raw := []string{
+		"apiVersion",
+		"kind",
+		"metadata.name",
+		"metadata.namespace",
+		"markhorParams.hierarchySeparator",
+		"markhorParams.managedAnnotation",
+		"markhorParams.order",
+		"type",
+		"data.sessionSecret",
+		"stringData.another",
+	}
+
+	expectedResult := []Ordering{
+		{Name: "apiVersion", Terms: []Ordering{}},
+		{Name: "kind", Terms: []Ordering{}},
+		{Name: "metadata", Terms: []Ordering{
+			{Name: "name", Terms: []Ordering{}},
+			{Name: "namespace", Terms: []Ordering{}},
+		}},
+		{Name: "markhorParams", Terms: []Ordering{
+			{Name: "hierarchySeparator", Terms: []Ordering{}},
+			{Name: "managedAnnotation", Terms: []Ordering{}},
+			{Name: "order", Terms: []Ordering{}},
+		}},
+		{Name: "type", Terms: []Ordering{}},
+		{Name: "data", Terms: []Ordering{
+			{Name: "sessionSecret", Terms: []Ordering{}},
+		}},
+		{Name: "stringData", Terms: []Ordering{
+			{Name: "another", Terms: []Ordering{}},
+		}},
+	}
+
+	result, err := parseOrdering(raw, ".")
+	if err != nil {
+		t.Fatal("This error whould have not occcourred", err)
+	}
+	diff := cmp.Diff(expectedResult, result)
+	if diff != "" {
+		t.Errorf("%s", diff)
+	}
+}
+
+func TestParseOrdering_Invalid(t *testing.T) {
+	t.Parallel()
+	inputs := [][]string{
+		{
+			"apiVersion",
+			"kind",
+			"metadata.name",
+			"metadata.namespace",
+			"markhorParams.hierarchySeparator",
+			"markhorParams.managedAnnotation",
+			"markhorParams.order",
+			"markhorParams", //culprit
+			"type",
+		},
+		{
+			"apiVersion",
+			"kind",
+			"metadata", //culprit
+			"metadata.name",
+			"metadata.namespace",
+			"markhorParams.hierarchySeparator",
+			"markhorParams.managedAnnotation",
+			"markhorParams.order",
+			"type",
+		},
+	}
+	for i, input := range inputs {
+		innerinput := input
+		t.Run(fmt.Sprint("Input number ", i, " (starting form 0)"), func(t *testing.T) {
+			t.Parallel()
+			_, err := parseOrdering(innerinput, ".")
+			if err == nil {
+				t.Fatal("This operation should have failed")
+			}
+		})
+	}
+}
+
+func TestSortWithOrdering(t *testing.T) {
+	t.Parallel()
+	t.Run("Successful ordering with valid simple input", func(t *testing.T) {
+		t.Parallel()
+		jsonData := map[string]interface{}{
+			"c": 3,
+			"b": 1,
+			"a": 2,
+		}
+		ordering := Ordering{
+			Name: "",
+			Terms: []Ordering{
+				{Name: "a", Terms: nil},
+				{Name: "b", Terms: nil},
+				{Name: "c", Terms: nil},
+			},
+		}
+		expected := orderedmap.New[string, interface{}]()
+		expected.Set("a", 2)
+		expected.Set("b", 1)
+		expected.Set("c", 3)
+
+		result, err := sortWithOrdering(jsonData, ordering)
+
+		if err != nil {
+			t.Errorf("Expected no error, but got %v", err)
+		}
+
+		if !reflect.DeepEqual(result, expected) {
+			t.Errorf("Result doesn't match the expected ordering")
+		}
+	})
+
+	t.Run("Successful ordering with valid complex input", func(t *testing.T) {
+		t.Parallel()
+		jsonData := map[string]interface{}{
+			"apiVersion": "markhor.example.com/v1",
+			"kind":       "MarkhorSecret",
+			"metadata": map[string]interface{}{
+				"namespace": "default",
+				"name":      "sample-secret",
+			},
+			"markhorParams": map[string]interface{}{
+				"order": []interface{}{
+					"apiVersion",
+					"kind",
+					"metadata/name",
+					"metadata/namespace",
+					"markhorParams/hierarchySeparator",
+					"markhorParams/managedAnnotation",
+					"markhorParams/order",
+					"type",
+					"data/session_secret",
+					"stringData/another",
+				},
+				"hierarchySeparator": "/",
+				"managedAnnotation":  "",
+			},
+			"type": "Opaque",
+			"stringData": map[string]interface{}{
+				"another": "I want some pineapples",
+			},
+			"data": map[string]interface{}{
+				"session_secret": "aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUT8=",
+			},
+		}
+
+		ordering := Ordering{
+			Name: "",
+			Terms: []Ordering{
+				{Name: "apiVersion", Terms: []Ordering{}},
+				{Name: "kind", Terms: []Ordering{}},
+				{Name: "metadata", Terms: []Ordering{
+					{Name: "name", Terms: []Ordering{}},
+					{Name: "namespace", Terms: []Ordering{}},
+				}},
+				{Name: "markhorParams", Terms: []Ordering{
+					{Name: "hierarchySeparator", Terms: []Ordering{}},
+					{Name: "managedAnnotation", Terms: []Ordering{}},
+					{Name: "order", Terms: []Ordering{}},
+				}},
+				{Name: "data", Terms: []Ordering{
+					{Name: "session_secret", Terms: []Ordering{}},
+				}},
+				{Name: "stringData", Terms: []Ordering{
+					{Name: "another", Terms: []Ordering{}},
+				}},
+				{Name: "type", Terms: []Ordering{}},
+			},
+		}
+
+		expected := orderedmap.New[string, interface{}]()
+		{
+			expected.Set("apiVersion", "markhor.example.com/v1")
+			expected.Set("kind", "MarkhorSecret")
+			metadata := orderedmap.New[string, interface{}]()
+			metadata.Set("name", "sample-secret")
+			metadata.Set("namespace", "default")
+			expected.Set("metadata", metadata)
+			markhorParams := orderedmap.New[string, interface{}]()
+			markhorParams.Set("hierarchySeparator", "/")
+			markhorParams.Set("managedAnnotation", "")
+			markhorParams.Set("order", []string{
+				"apiVersion",
+				"kind",
+				"metadata/name",
+				"metadata/namespace",
+				"markhorParams/hierarchySeparator",
+				"markhorParams/managedAnnotation",
+				"markhorParams/order",
+				"type",
+				"data/session_secret",
+				"stringData/another",
+			})
+			expected.Set("markhorParams", markhorParams)
+			data := orderedmap.New[string, interface{}]()
+			data.Set("session_secret", "aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUT8=")
+			expected.Set("data", data)
+			stringData := orderedmap.New[string, interface{}]()
+			stringData.Set("another", "I want some pineapples")
+			expected.Set("stringData", stringData)
+			expected.Set("type", "Opaque")
+		}
+
+		result, err := sortWithOrdering(jsonData, ordering)
+
+		if err != nil {
+			t.Errorf("Expected no error, but got %v", err)
+		}
+
+		m1, _ := json.Marshal(result)
+		m2, _ := json.Marshal(expected)
+		s1 := string(m1)
+		s2 := string(m2)
+		if s1 != s2 {
+			t.Errorf("Result doesn't match the expected ordering. Expected %s, got %s", s1, s2)
+		}
+	})
+
+	t.Run("Unsuccessful ordering with invalid input", func(t *testing.T) {
+		t.Parallel()
+		jsonData := map[string]interface{}{
+			"a": 2,
+			"b": 1,
+			"c": 3,
+		}
+		ordering := Ordering{
+			Name: "",
+			Terms: []Ordering{
+				{Name: "a", Terms: nil},
+				{Name: "b", Terms: nil},
+				{Name: "d", Terms: nil}, // 'd' key doesn't exist in jsonData
+			},
+		}
+
+		result, err := sortWithOrdering(jsonData, ordering)
+
+		if err == nil {
+			t.Error("Expected an error, but got none")
+		}
+
+		if result != nil {
+			t.Error("Expected result to be nil")
+		}
+	})
+}
+
+// Sorts according to the order and puts sops at the end
+func TestSortJson(t *testing.T) {
+	t.Parallel()
+	jsonData := map[string]interface{}{
+		"apiVersion": "markhor.example.com/v1",
+		"kind":       "MarkhorSecret",
+		"metadata": map[string]interface{}{
+			"namespace": "default",
+			"name":      "sample-secret",
+		},
+		"markhorParams": map[string]interface{}{
+			"order": []string{
+				"apiVersion",
+				"kind",
+				"metadata/name",
+				"metadata/namespace",
+				"markhorParams/hierarchySeparator",
+				"markhorParams/managedAnnotation",
+				"markhorParams/order",
+				"type",
+				"data/session_secret",
+				"stringData/another",
+			},
+			"hierarchySeparator": "/",
+			"managedAnnotation":  "",
+		},
+		"sops": map[string]interface{}{
+			"something": "values",
+		},
+		"type": "Opaque",
+		"stringData": map[string]interface{}{
+			"another": "I want some pineapples",
+		},
+		"data": map[string]interface{}{
+			"session_secret": "aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUT8=",
+		},
+	}
+
+	expected := orderedmap.New[string, interface{}]()
+	{
+		expected.Set("apiVersion", "markhor.example.com/v1")
+		expected.Set("kind", "MarkhorSecret")
+		metadata := orderedmap.New[string, interface{}]()
+		metadata.Set("name", "sample-secret")
+		metadata.Set("namespace", "default")
+		expected.Set("metadata", metadata)
+		markhorParams := orderedmap.New[string, interface{}]()
+		markhorParams.Set("hierarchySeparator", "/")
+		markhorParams.Set("managedAnnotation", "")
+		markhorParams.Set("order", []string{
+			"apiVersion",
+			"kind",
+			"metadata/name",
+			"metadata/namespace",
+			"markhorParams/hierarchySeparator",
+			"markhorParams/managedAnnotation",
+			"markhorParams/order",
+			"type",
+			"data/session_secret",
+			"stringData/another",
+		})
+		expected.Set("markhorParams", markhorParams)
+		data := orderedmap.New[string, interface{}]()
+		expected.Set("type", "Opaque")
+		data.Set("session_secret", "aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUT8=")
+		expected.Set("data", data)
+		stringData := orderedmap.New[string, interface{}]()
+		stringData.Set("another", "I want some pineapples")
+		expected.Set("stringData", stringData)
+		sops := orderedmap.New[string, interface{}]()
+		sops.Set("something", "values")
+		expected.Set("sops", sops)
+	}
+
+	result := sortJson(jsonData, slog.String("", ""))
+
+	m1, _ := json.Marshal(result)
+	m2, _ := json.Marshal(expected)
+	s1 := string(m1)
+	s2 := string(m2)
+	if s1 != s2 {
+		t.Errorf("Result doesn't match the expected ordering. Expected %s, got %s", s1, s2)
+	}
+}
