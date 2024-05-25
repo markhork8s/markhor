4d3d9bb61b497fdb57696b9b732753f80d55a59f
civts <24255608+civts@users.noreply.github.com>
2024-05-25 13:37:08 +0200

    Version 1, here we go

--- pkg/costants.go
+++ pkg/costants.go
@@ -1,7 +1,7 @@
 package pkg
 
 // Version of this package (Markhor)
-const VERSION string = "0.0.12"
+const VERSION string = "1.0.0"
 
 // In a MarkhorSecret, this key contains the Markhor parameters
 const MARKHORPARAMS_MANIFEST_KEY string = "markhorParams"
